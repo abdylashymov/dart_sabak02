13ed99488f0bd09bb626a682ca64a378c8b1a634
Elchibek <abdylashymov@gmail.com>
2024-08-26 14:51:36 +0600

    dart themes added

---
+++ lib/dart_variables.dart
@@ -0,0 +1,63 @@
+void main() {
+  String name = 'Elchibek';
+  String surname = 'Abdylashymov';
+  String company = 'APP ITC';
+  String city = 'Bishkek';
+  String town = 'Razzakov';
+  print(name);
+  print(surname);
+  print(company);
+  print(city);
+  print(town);
+}
+
+void numbers(){
+num year = 1991;
+num year1 = 1997;
+num year2 = 2015;
+num year3 = 2016;
+print(year);
+print(year1);
+print(year2);
+print(year3);
+}
+
+void integers(){
+  int san = 1;
+  int san1 = 2;
+  int san2 = 3;
+  int san3 = 4;
+  print(san);
+  print(san1);
+  print(san2);
+  print(san3);
+  }
+  void doubles(){
+    double height = 1.3;
+    double weight = 1.4;
+    double height1 = 2.5;
+    double weight2 = 45.6;
+    print(height);
+    print(height1);
+    print(weight2);
+    print(weight);
+  }
+void booleans(){
+  bool isMarried = true;
+  bool isMarried2 = false;
+  print(isMarried2);
+  print(isMarried);
+}
+dynamic baaryBolot(){
+  dynamic int = 1;
+  dynamic num = 2;
+  dynamic string = 'text';
+  dynamic bool = true;
+  dynamic double = 1.4;
+}
+
+const pi = 3.14;
+var lastName = 'Abdylashymov Elchibek';
+final data = 2002; 
+
+
